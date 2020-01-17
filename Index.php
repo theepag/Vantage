0c85dd052f46a48228367356c6b5a609dede7630
theepag <theepag@live.com>
2020-01-17 22:00:48 +0530

    Initial Commit

---
+++ Index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hotel Vantage</title>
+	
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700" rel="stylesheet">
+	
+	<!-- Template Styles -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	
+	<!-- CSS Reset -->
+	<link rel="stylesheet" href="css/normalize.css">
+	
+	<!-- Milligram CSS minified -->
+	<link rel="stylesheet" href="css/milligram.min.css">
+	
+	<!-- Main Styles -->
+	<link rel="stylesheet" href="css/styles.css">
+	
+</head>
+
+<body>
+	<div class="navbar">
+		<div class="row">
+                    <div class="column column-30 col-site-title"><a href="#" class="site-title float-left"><img src="Images/logo.png"></a></div>
+			<div class="column column-40 col-search"><a href="#" class="search-btn fa fa-search"></a>
+				<input type="text" name="" value="" placeholder="Search..." />
+			</div>
+			<div class="column column-30">
+				<div class="user-section"><a href="#">
+					<img src="http://via.placeholder.com/50x50" alt="profile photo" class="circle float-left profile-photo" width="50" height="auto">
+					<div class="username">
+						<h4>Jane Donovan</h4>
+						<p>Administrator</p>
+                                                
+					</div>
+				</a></div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="sidebar" class="column">
+			<h5>Navigation</h5>
+			<ul>
+				<li><a href="#"><em class="fa fa-home"></em>    Meals</a></li>
+				<li><a href="#charts"><em class="fa fa-bar-chart"></em> Orders</a></li>
+				<li><a href="#widgets"><em class="fa fa fa-clone"></em> Customer</a></li>
+				<li><a href="#forms"><em class="fa fa-pencil-square-o"></em> Feedbacks</a></li>
+				<li><a href="#alerts"><em class="fa fa-warning"></em> Staff</a></li>
+				<li><a href="#buttons"><em class="fa fa-hand-o-up"></em> Buttons</a></li>
+				<li><a href="#tables"><em class="fa fa-table"></em> Tables</a></li>
+				<li><a href="#grid"><em class="fa fa-columns"></em> Grid</a></li>
+			</ul>
+		</div>
+                    <section id="main-content" class="column column-offset-20">
+
+                            <!--Meals-->
+                            <h5 class="mt-2">Meals</h5><a class="anchor" name="meals"></a>
+                            <div class="row grid-responsive">
+                                    <div class="column ">
+                                            <div class="card">
+                                                    <div class="card-title">
+                                                            <h3>Available Meals</h3>
+                                                    </div>
+                                                    <div class="card-block">
+                                                            <table>
+                                                                    <thead>
+                                                                            <tr>
+                                                                                    <th>ID</th>
+                                                                                    <th>Name</th>
+                                                                                    <th>Description</th>
+                                                                                    <th>Image</th>
+                                                                                    <th>Type</th>
+                                                                                    <th>Price</th>
+                                                                            </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                            <tr>
+                                                                                    <td>Jane Donovan</td>
+                                                                                    <td>UI Developer</td>
+                                                                                    <td>23</td>
+                                                                                    <td>Philadelphia, PA</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                    <td>Jonathan Smith</td>
+                                                                                    <td>Designer</td>
+                                                                                    <td>30</td>
+                                                                                    <td>London, UK</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                    <td>Kelly Johnson</td>
+                                                                                    <td>UX Developer</td>
+                                                                                    <td>25</td>
+                                                                                    <td>Los Angeles, CA</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                    <td>Sam Davidson</td>
+                                                                                    <td>Programmer</td>
+                                                                                    <td>28</td>
+                                                                                    <td>Philadelphia, PA</td>
+                                                                            </tr>
+                                                                    </tbody>
+                                                            </table>
+                                                    </div>
+                                            </div>
+                                    </div>
+                            </div>
+
+                           
+                    </section>
+	</div>
+				
+
+</body>
+</html> 
